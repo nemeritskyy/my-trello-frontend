6d02b29fe86aae501b56120b486531382f0a7d8b
Andrii Nemeritskyy <andrey.nemeritskyy@gmail.com>
2025-02-04 14:59:27 +0200

    Change deploy branch

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git add -A
 git commit -m 'deploy'
 
 
-git push -f git@github.com:nemeritskyy/my-trello-frontend.git master:gh-pages
+git push -f git@github.com:nemeritskyy/my-trello-frontend.git develop:gh-pages
 
 cd -
